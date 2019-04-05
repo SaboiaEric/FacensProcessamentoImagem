c8db42ffa0ce54b9d88e65d4708e2e39b9b3633f
Eric Saboia <saboia.eric@gmail.com>
2019-04-04 21:26:13 -0300

    Adicionando versoes nos codigos

---
+++ Projeto_2/quadrados.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+#Eric Saboia - 140730
+#Guilherme Panayotou - 140114
+
+dados =  np.array([[1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 0, 0, 1, 1, 1, 1, 1, 1],
+                  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+                  [0, 0, 0, 0, 1, 1, 1, 0, 1, 1],
+                  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+                  [0, 0, 0, 1, 1, 1, 1, 1, 1, 0]], dtype = np.uint8)
+
+kernel = np.array([[1, 1, 1],
+                   [1, 1, 1],
+                   [1, 1, 1]], dtype = np.uint8)
+
+saida = np.zeros((10,10), dtype = np.uint8)
+
+for col in range(0, len(dados[0]), 3):
+    for lin in range(0, len(dados), 1):
+        if(dados[col:col+3,lin:lin+3].shape == kernel[:,:].shape):
+            if(np.array_equal(dados[col:col+3,lin:lin+3], kernel[:,:])):
+                saida[col+1,lin+1] = 1
+
+print("Total de erosões realizadas: %d"%np.count_nonzero(saida))
+
+kernel_2 = np.array([1, 1, 1, 1], dtype = np.uint8)
+
+for lin in range(0, len(saida[0]), 3):
+    for col in range(0, len(saida), 1):
+        if(saida[col,lin:lin+4].shape == kernel_2.shape):
+            if(np.array_equal(saida[lin+1,col:col+4], kernel_2)):
+                saida[lin+1, col+1] = 0
+                saida[lin+1, col+2] = 0
+
+print("Quadrados existentes  %d"%np.count_nonzero(saida))
